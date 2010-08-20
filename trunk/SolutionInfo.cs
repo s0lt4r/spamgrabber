77dd1e8124116a081891bcc1d1ef87e2557cb1f2
lmayne <lmayne@402fe06f-58b1-2c0d-ef12-22603fea4fc1>
2010-08-20 19:52:02 +0000

    Removed tagged version

--- trunk/SolutionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("4.0.8")]
-[assembly: AssemblyFileVersion("4.0.8.0")]
